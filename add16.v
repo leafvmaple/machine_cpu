fafc28485377d43e2dd38948a9414ced18935a96
Zohar Lee <root@leafvmaple.com>
2019-09-24 03:18:05 +0800

    modify to bcla

--- add16.v
+++ add16.v
@@ -10,12 +10,19 @@ module Add16(out, a, b);
 output [15:0] out;
 input [15:0] a, b;
 
-wire c4, c8, c12, c16;
+wire [3:0] c, g, p;
 
-CLA4 cla0(out[3:0],   c4,  a[3:0],   b[3:0],   1'b0);
-CLA4 cla1(out[7:4],   c8,  a[7:4],   b[7:4],   c4);
-CLA4 cla2(out[11:8],  c12, a[11:8],  b[11:8],  c8);
-CLA4 cla3(out[15:12], c16, a[15:12], b[15:12], c12);
+CLABase4 clabase(c, g, p, c0);
+
+BCLA4 bcla0(out[3:0],   g[0], p[0], a[3:0],   b[3:0],   1'b0);
+BCLA4 bcla1(out[7:4],   g[1], p[1], a[7:4],   b[7:4],   c[0]);
+BCLA4 bcla2(out[11:8],  g[2], p[2], a[11:8],  b[11:8],  c[1]);
+BCLA4 bcla3(out[15:12], g[3], p[3], a[15:12], b[15:12], c[2]);
+
+/*CLA4 cla0(out[3:0],   c[0], a[3:0],   b[3:0],   1'b0);
+CLA4 cla1(out[7:4],   c[1], a[7:4],   b[7:4],   c[0]);
+CLA4 cla2(out[11:8],  c[2], a[11:8],  b[11:8],  c[1]);
+CLA4 cla3(out[15:12], c[3], a[15:12], b[15:12], c[2]);*/
 
 endmodule
 
--- cla4.v
+++ cla4.v
@@ -32,11 +32,35 @@ assign p = a | b;
 
 CLABase4 clabase(c, g, p, c0);
 
-assign sum[0] = a[0] ^ b[0];
+assign sum[0] = a[0] ^ b[0] ^ c0;
 assign sum[1] = a[1] ^ b[1] ^ c[0];
 assign sum[2] = a[2] ^ b[2] ^ c[1];
 assign sum[3] = a[3] ^ b[3] ^ c[2]; // 4 Gate Delay
 assign c4 = c[3];
+
+endmodule
+
+module BCLA4(sum, bg, bp, a, b, c0);
+
+output [3:0] sum;
+output bg, bp;
+input [3:0] a, b;
+input c0;
+
+wire [3:0] g, p, c;
+
+assign g = a & b;
+assign p = a | b;
+
+CLABase4 clabase(c, g, p, c0);
+
+assign bg = g[3] | p[3] & g[2] | p[3] & p[2] & g[1] | p[3] & p[2] & p[1] & g[0];
+assign bp = p[3] & p[2] & p[1] & p[0];
+
+assign sum[0] = a[0] ^ b[0] ^ c0;
+assign sum[1] = a[1] ^ b[1] ^ c[0];
+assign sum[2] = a[2] ^ b[2] ^ c[1];
+assign sum[3] = a[3] ^ b[3] ^ c[2]; // 4 Gate Delay
 
 endmodule
 
