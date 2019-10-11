9ca1a4a09baf1d4bb9b8882e01a781b7e0d33a47
Zohar Lee <root@leafvmaple.com>
2019-10-11 21:43:51 +0800

    Using Cache Memory in CPU instead of Main Memory

--- src/cpu_top.v
+++ src/cpu_top.v
@@ -7,29 +7,33 @@
 `include "reg.v"
 `include "pc.v"
 `include "ir.v"
+`include "mem.v"
 
-module CPU(addr_bus, data_bus_out, mem_read, mem_wrt, data_bus_in);
-
-output [31:0] addr_bus;
-output [31:0] data_bus_out;
-output mem_read, mem_wrt;
-input [31:0] data_bus_in;
+module CPU;
 
 reg clk, pc_clk;
 
+// IF
 wire [31:0] inst, addr_inst;
 
+// ID
 wire [4:0] rs, rt, rd;
 wire [3:0] alu_ctr_sig;
-wire reg_dst_sig, reg_wrt_sig, mem_reg_sig, alu_src_sig, branch_sig, jump_sig;
-
+wire reg_dst_sig, reg_wrt_sig, mem_read_rig, mem_wrt_rig, mem_reg_sig, alu_src_sig, branch_sig, jump_sig;
 wire [31:0] src_data, rt_data;
 
 wire [15:0] imm16_data;
 wire [25:0] imm26_data;
 
+// EX
 wire zf;
 wire [31:0] alu_out;
+
+// MEM
+wire [31:0] mem_out;
+
+// WB
+wire [31:0] back_data;
 
 
 /*Initial*/
@@ -43,26 +47,26 @@ always #1 clk = ~clk;
 
 always @(clk) #4 pc_clk = ~pc_clk;
 
-////////////////////////
-/// Load Instruction ///
-////////////////////////
+/////////////////////////
+/// Instruction Fetch ///
+/////////////////////////
 
-/*Program Counter*/
+/* Program Counter */
 PC pc(addr_inst, zf & branch_sig, jump_sig, imm16_data, imm26_data, pc_clk);
 
-/*Instruction Register*/
+/* Instruction Register */
 IRegister ir(inst, addr_inst, clk);
 
-////////////////////////
-///      Decode      ///
-////////////////////////
+//////////////////////////
+/// Instruction Decode ///
+//////////////////////////
 
 /* Decoder */
 Decoder decoder(alu_ctr_sig,
                 reg_dst_sig,
                 reg_wrt_sig,
-                mem_read,
-                mem_wrt,
+                mem_read_rig,
+                mem_wrt_rig,
                 mem_reg_sig,
                 alu_src_sig,
                 branch_sig,
@@ -72,10 +76,6 @@ Decoder decoder(alu_ctr_sig,
                 rd,
                 inst);
 
-////////////////////
-/// Visit Memery ///
-////////////////////
-
 /* Register */
 Register register(src_data,
                   rt_data,
@@ -83,22 +83,28 @@ Register register(src_data,
                   rs,
                   rt,
                   reg_dst_sig ? rd : rt,
-                  mem_reg_sig ? data_bus_in : alu_out,
+                  back_data,
                   clk);
 
 assign imm16_data = inst[15:0];
 assign imm26_data = inst[25:0];
-assign data_bus_out = rt_data;
 
-////////////////////////
-///      Excute      ///
-////////////////////////
+////////////////////
+///    Excute    ///
+////////////////////
 ALU alu(alu_out, zf, alu_ctr_sig, src_data, alu_src_sig ? imm16_data : rt_data);
+
+////////////////////
+///    Memery    ///
+////////////////////
+
+/* Cache Memory */
+Memory mem(mem_out, rt_data, alu_out, mem_read_rig, mem_wrt_rig, clk);
 
 ////////////////////////
 ///    Write Back    ///
 ////////////////////////
-assign addr_bus = alu_out;
+assign back_data = mem_reg_sig ? mem_out : alu_out;
 
 endmodule
 
--- src/machine.v
+++ src/machine.v
@@ -4,25 +4,10 @@
 `define _MACHINE_V_ 
 
 `include "cpu_top.v"
-`include "mem.v"
 
 module Board;
 
-wire [31:0] addr_bus;
-wire [31:0] data_bus_up, data_bus_down;
-
-reg clk;
-
-initial
-begin
-    clk = 0;
-end
-
-always #1 clk = ~clk;
-
-Memory mem(data_bus_up, data_bus_down, addr_bus, mem_read, mem_wrt, clk);
-
-CPU cpu(addr_bus, data_bus_down, mem_read, mem_wrt, data_bus_up);
+CPU cpu();
 
 endmodule
 
