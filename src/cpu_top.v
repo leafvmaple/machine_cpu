0c4bb80c61511f0dc3ccc5c46e09a395fc1a8bce
Zohar Lee <root@leafvmaple.com>
2019-10-18 17:39:43 +0800

    And Part Register in CPU

--- src/cpu_top.v
+++ src/cpu_top.v
@@ -17,6 +17,8 @@ reg clk, pc_clk;
 wire [31:0] pc_out, pc_added;
 wire [31:0] inst;
 
+reg [31:0] pc_added_id, inst_id;
+
 // ID
 wire [4:0] rs, rt, rd;
 wire [3:0] alu_ctr_sig;
@@ -28,17 +30,25 @@ wire [25:0] imm26_data;
 wire [31:0] imm32_data;
 wire [31:0] pc_jump;
 
+reg [31:0] src_data_ex, rt_data_ex;
+reg [31:0] imm32_data_ex, pc_added_ex, pc_jump_ex;
+
 // EX
 wire zf;
 wire [31:0] alu_out;
 wire [31:0] pc_branched, pc_back;
 
+reg [31:0] rt_data_mem;
+reg [31:0] alu_out_mem;
+
 // MEM
 wire [31:0] mem_out;
 
+reg [31:0] alu_out_wb;
+reg [31:0] mem_out_wb;
+
 // WB
 wire [31:0] back_data;
-
 
 /*Initial*/
 initial
@@ -63,6 +73,11 @@ assign pc_added = pc_out + 4;
 /* Instruction Register */
 IRegister ir(inst, pc_out, clk);
 
+always @(posedge clk) begin
+    pc_added_id = pc_added;
+    inst_id = inst;
+end
+
 //////////////////////////
 /// Instruction Decode ///
 //////////////////////////
@@ -80,7 +95,7 @@ Decoder decoder(alu_ctr_sig,
                 rs,
                 rt,
                 rd,
-                inst);
+                inst_id);
 
 /* Register */
 Register register(src_data,
@@ -92,31 +107,49 @@ Register register(src_data,
                   back_data,
                   clk);
 
-assign imm16_data = inst[15:0];
-assign imm26_data = inst[25:0];
+assign imm16_data = inst_id[15:0];
+assign imm26_data = inst_id[25:0];
 
 assign imm32_data = {{16{imm16_data[15]}}, imm16_data};
-assign pc_jump = {pc_added[31:28], imm26_data << 2};
+assign pc_jump = {pc_added_id[31:28], imm26_data << 2};
+
+always @(posedge clk) begin
+    src_data_ex = src_data;
+    rt_data_ex = rt_data;
+    imm32_data_ex = imm32_data;
+    pc_added_ex = pc_added_id;
+    pc_jump_ex = pc_jump;
+end
 
 ////////////////////
 ///    Excute    ///
 ////////////////////
-ALU alu(alu_out, zf, alu_ctr_sig, src_data, alu_src_sig ? imm32_data : rt_data);
+ALU alu(alu_out, zf, alu_ctr_sig, src_data_ex, alu_src_sig ? imm32_data_ex : rt_data_ex);
 
-assign pc_branched = (zf & branch_sig)? (pc_added + imm32_data << 2) : pc_added;
-assign pc_back = jump_sig ? pc_jump : pc_branched;
+assign pc_branched = (zf & branch_sig)? (pc_added_ex + imm32_data_ex << 2) : pc_added_ex;
+assign pc_back = jump_sig ? pc_jump_ex : pc_branched;
+
+always @(posedge clk) begin
+    alu_out_mem = alu_out;
+    rt_data_mem = rt_data_ex;
+end
 
 ////////////////////
 ///    Memery    ///
 ////////////////////
 
 /* Cache Memory */
-Memory mem(mem_out, rt_data, alu_out, mem_read_rig, mem_wrt_rig, clk);
+Memory mem(mem_out, rt_data_mem, alu_out_mem, mem_read_rig, mem_wrt_rig, clk);
+
+always @(posedge clk) begin
+    mem_out_wb = mem_out;
+    alu_out_wb = alu_out_mem;
+end
 
 ////////////////////////
 ///    Write Back    ///
 ////////////////////////
-assign back_data = mem_reg_sig ? mem_out : alu_out;
+assign back_data = mem_reg_sig ? mem_out_wb : alu_out_wb;
 
 endmodule
 
